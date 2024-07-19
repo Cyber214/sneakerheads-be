bae714abb6ea4a68b8ddd04d0b197ae92a07569a
Seun Dada <seun.dada777@gmail.com>
2024-07-19 16:32:28 -0400

    add website url to allowed hosts

--- sneakerheads/settings.py
+++ sneakerheads/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-z2@*1=2cc$&7o7ig&ds+q64jrdw_&9($qm^q^e)3b7q@dh9__m
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['3.141.34.115', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['3.141.34.115', 'localhost', '127.0.0.1', 'www.sneakerheads.live.s3-website.us-east-2.amazonaws.com']
 
 # Application definition
 
